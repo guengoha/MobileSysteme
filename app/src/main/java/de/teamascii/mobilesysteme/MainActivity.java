61afc50555f9c6d90e4564e0b3b9f4ca0367c8f3
tructran <truc.trn@yahoo.com>
2019-04-11 16:00:56 +0200

    Hello World Bros

--- app/src/main/java/de/teamascii/mobilesysteme/MainActivity.java
+++ app/src/main/java/de/teamascii/mobilesysteme/MainActivity.java
@@ -2,6 +2,7 @@ package de.teamascii.mobilesysteme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView welcomeMessageTV = new TextView(this);
+        welcomeMessageTV.setText("Hello CodeYourApp World!");
+        setContentView(welcomeMessageTV);
+
     }
 }
