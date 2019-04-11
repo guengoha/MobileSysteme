8a51d390b2c0fdf127b67c9d44ce4160a481d909
itzuern <stefanwilshere@googlemail.com>
2019-04-11 15:28:55 +0200

    Created MainActivity

---
+++ app/src/main/java/de/teamascii/mobilesysteme/MainActivity.java
@@ -0,0 +1,13 @@
+package de.teamascii.mobilesysteme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
